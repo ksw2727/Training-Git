7eadd56cdff9d9e67e9387d94e5289196b72754a
Kevin Whelan <Kevin.Whelan@encore.tech>
2019-08-05 18:39:32 -0400

    cockpitmodule folder added

---
+++ assignment5/cockpitmodule/cockpit/manifests/init.pp
@@ -0,0 +1,18 @@
+class cockpit {
+         package { 'cockpit'                                         :
+                 ensure => present                                   ,
+         }
+
+         file    { '/etc/cockpit/cockpit.conf'                       :
+                 ensure     =>  file                                 ,
+                 content    =>  template('cockpit/cockpit.conf.erb') ,
+                 require    =>  Package['cockpit']                   ,
+         }
+
+         service { 'cockpit'                                         :
+                 ensure     =>  running                              ,
+                 enable     =>  true                                 ,
+                 hasrestart =>  true                                 ,
+                 require    =>  [ File['/etc/cockpit/cockpit.conf'] ],
+         }
+}
